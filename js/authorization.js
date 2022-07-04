9579678c4bddfe27077239d0b945b200210f0504
Roman Kuzmenko <83546337+KuzmenkoRomanJS@users.noreply.github.com>
2022-07-04 17:48:54 +0300

    Update authorization.js

--- js/authorization.js
+++ js/authorization.js
@@ -61,6 +61,8 @@ const logout = (e) => {
     localStorage.removeItem('Bearer');
     btn.textContent = '';
     btn.style.backgroundImage = '';
+    btn.removeEventListener('click', logout);
+    btn.addEventListener('click', login);
   }
 }
 
